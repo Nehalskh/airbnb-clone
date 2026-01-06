cbd43140cd90bcd41f73ae6b37823760ba572703
Nehal Shaikh <113497710+Nehalskh@users.noreply.github.com>
2026-01-06 15:08:14 +0530

    Implement session management with MongoDB
    Added session management using express-session and MongoDBStore.

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 const path = require("path");
 //External Modules
 const express = require("express");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const DB_PATH =
+  "mongodb+srv://nehalshaikh8664_db_user:yPJDG8qPVlwskFZ4@nehalskhcluster.xtyv7bd.mongodb.net/airbnb?appName=NehalskhCluster";
 
 //Local module
 const storeRouter = require("./routes/storeRouter");
@@ -16,13 +20,20 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
-app.use((req, res, next) => {
-  console.log("Cookie check middleware", req.get("Cookie"));
-  req.isLoggedIn = req.get("cookie")
-    ? req.get("Cookie").split("=")[1] === "true"
-    : "false";
-  next();
+// Session store config
+const store = new MongoDBStore({
+  uri: DB_PATH,
+  collection: "sessions",
 });
+
+app.use(
+  session({
+    secret: "alexander the great",
+    resave: false,
+    saveUninitialized: true,
+    store,
+  })
+);
 
 // ✔️ MOVE STATIC BEFORE ROUTERS
 app.use(express.static(path.join(rootDir, "public")));
@@ -31,7 +42,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(authRouter);
 app.use(storeRouter);
 app.use("/host", (req, res, next) => {
-  if (req.isLoggedIn) {
+  if (req.session.isLoggedIn) {
     next();
   } else {
     res.redirect("/login");
@@ -43,8 +54,6 @@ app.use("/host", hostRouter);
 app.use(errorController.pageNotFound);
 
 const PORT = 3002;
-const DB_PATH =
-  "mongodb+srv://nehalshaikh8664_db_user:yPJDG8qPVlwskFZ4@nehalskhcluster.xtyv7bd.mongodb.net/airbnb?appName=NehalskhCluster";
 
 mongoose
   .connect(DB_PATH)
